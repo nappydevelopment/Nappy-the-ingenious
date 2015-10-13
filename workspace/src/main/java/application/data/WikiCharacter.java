4df9e6ca7375158453fad8585b7c594f882e1b1e
Manuel Bothner <manuel.bothner@web.de>
2015-10-13 11:25:25 +0200

    Frontend init. commit

---
+++ workspace/src/main/java/application/data/WikiCharacter.java
@@ -0,0 +1,44 @@
+//### WikiCharacter.java ###################################################################################################################
+package application.data;
+
+//### IMPORTS ##############################################################################################################################
+import javafx.scene.image.Image;
+
+public class WikiCharacter {
+	
+//### ATTRIBUTES ###########################################################################################################################
+	
+	private String name;
+	private String nickname;
+	private String description;
+	private Image wikiImage;
+
+//### CONSTRUCTORS #########################################################################################################################
+	
+	public WikiCharacter(String name, String nickname, String description, Image wikiImage) {
+		this.name = name;
+		this.nickname = nickname;
+		this.description = description;
+		this.wikiImage = wikiImage;
+	}
+	
+//### GETTER/SETTER ########################################################################################################################
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public Image getWikiImage() {
+		return wikiImage;
+	}
+	
+}
+//### EOF ##################################################################################################################################
