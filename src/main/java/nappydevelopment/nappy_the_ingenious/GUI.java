7c995af9edbbc17983d54e8f1f1ef87e5280a0fb
Manuel Bothner <manuel.bothner@web.de>
2015-11-13 08:29:24 +0100

    began to implemented the MVC concept!

--- src/main/java/nappydevelopment/nappy_the_ingenious/GUI.java
+++ src/main/java/nappydevelopment/nappy_the_ingenious/GUI.java
@@ -2,14 +2,15 @@ package nappydevelopment.nappy_the_ingenious;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import nappydevelopment.nappy_the_ingenious.gui.MainStage;
+import nappydevelopment.nappy_the_ingenious.gui.MainStageController;
+import nappydevelopment.nappy_the_ingenious.gui.fxml.MainStage;
 
 //### IMPORTS ##############################################################################################################################
 public class GUI extends Application {
 	
 //### STAGES ###############################################################################################################################
 	
-	private MainStage mainStage;
+	private MainStageController mainStageController;
 	private Controller controller;
 
 //### GETTER / SETTER ######################################################################################################################	
@@ -21,14 +22,22 @@ public class GUI extends Application {
 //### PUBLIC METHODS #######################################################################################################################
 	
 	
-	
-	
 	@Override
+	//Method that starts the JavaFX-Application:
 	public void start(Stage stage) throws Exception {
 		
-		this.mainStage = new MainStage();
-		stage = this.mainStage;
-		stage.show();
+		//Create a new main-stage-controller:
+		this.mainStageController = new MainStageController();
+		
+		//Initialize the view of the main stage (here as separate step!):
+		this.mainStageController.initView();
+		
+		//Show the main-stage-window:
+		this.mainStageController.showStartView();
+	}
+	
+	public void stop() {
+		System.out.println("Exit");
 	}
 	
 }
