ced4e5de91adf03af8fa85b5fe924e987fd31392
MZ <>
2015-12-03 13:49:40 +0100

    Character provider load images; ugh FIX Images

--- src/main/java/nappydevelopment/nappy_the_ingenious/data/CharacterProvider.java
+++ src/main/java/nappydevelopment/nappy_the_ingenious/data/CharacterProvider.java
@@ -28,14 +28,7 @@ public class CharacterProvider{
 				String nickname = res.getString("nickname");
 				String description_en = res.getString("description_en");
 				String description_de = res.getString("description_de");
-				Image img = new Image(GlobalReferences.IMAGES_PATH +"wiki/"+ name.toLowerCase().replace(" ", "_") +".png");
-				//Image img = res.getObject("image", javafx.scene.image.Image.class); // probably bullshit and broken
-				/*try{
-					BufferedImage bImg = ImageIO.read(image);
-					img = SwingFXUtils.toFXImage(bImg, null);
-				}catch(IOException e){
-					e.printStackTrace();
-				}*/
+				Image img = new Image(GlobalReferences.IMAGES_PATH +"/wiki/"+ name.toLowerCase().replace(" ", "_") +".png");
 
 				WikiCharacter chr = null;
 				if(lang.equals(Language.GERMAN)){
