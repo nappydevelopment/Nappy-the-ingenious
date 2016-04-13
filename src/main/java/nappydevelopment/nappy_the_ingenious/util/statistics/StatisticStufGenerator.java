8e647f36c1c4a44cc890c8f2328c92529a7a7234
Uindemia <marc@m-mahler.de>
2016-04-13 15:23:31 +0200

    Auslesen der Top 5 Charaktere implementiert

--- src/main/java/nappydevelopment/nappy_the_ingenious/util/statistics/StatisticStufGenerator.java
+++ src/main/java/nappydevelopment/nappy_the_ingenious/util/statistics/StatisticStufGenerator.java
@@ -3,6 +3,7 @@ package nappydevelopment.nappy_the_ingenious.util.statistics;
 import com.sun.xml.internal.bind.v2.TODO;
 import nappydevelopment.nappy_the_ingenious.data.DatabaseProvider;
 import nappydevelopment.nappy_the_ingenious.data.Player;
+import nappydevelopment.nappy_the_ingenious.data.StatisticCharacter;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,11 +14,11 @@ import java.util.Comparator;
 /**
  * Created by Marc on 05.04.2016.
  */
-public class TopFiveGenerator {
+public class StatisticStufGenerator {
 
 
 
-    public TopFiveGenerator(){
+    public StatisticStufGenerator(){
 
     }
 
@@ -26,21 +27,40 @@ public class TopFiveGenerator {
         try{
             Statement st = DatabaseProvider.getStatement();
             st.execute(
-                     /* TODO
-                    * Select anhand zu erläuternder Kriterien erweitern */
-                    "SELECT * FROM HIGHSCORES ORDER BY score DESC LIMIT 5"
+                    "SELECT player_name, questions_nappy, questions_player, score FROM HIGHSCORES ORDER BY score DESC LIMIT 5"
             );
             ResultSet res = st.getResultSet();
 
             while(res.next()){
-                 /* TODO
-            * Nötige Dinge anhand Absprache aus dem RS auslesen */
                 String name = res.getString("player_name");
                 System.out.print(name+ ", ");
                 int nappy_fragen = res.getInt("questions_nappy");
                 int spieler_fragen = res.getInt("questions_player");
                 int punktzahl = res.getInt("score");
                 topFiveList.add(new Player(name, nappy_fragen, spieler_fragen, punktzahl));
+            }
+            res.close();
+            st.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        System.out.print("\n");
+        return topFiveList;
+    }
+
+    public ArrayList<StatisticCharacter> getTopFivePlayedCharacters(){
+        ArrayList<StatisticCharacter> topFiveList = new ArrayList<StatisticCharacter>();
+        try{
+            Statement st = DatabaseProvider.getStatement();
+            st.execute(
+                    "SELECT name, counter FROM SIMPSONS ORDER BY counter DESC LIMIT 5"
+            );
+            ResultSet res = st.getResultSet();
+
+            while(res.next()){
+                String name = res.getString("name");
+                int counter = res.getInt("counter");
+                topFiveList.add(new StatisticCharacter(name, counter));
             }
             res.close();
             st.close();
