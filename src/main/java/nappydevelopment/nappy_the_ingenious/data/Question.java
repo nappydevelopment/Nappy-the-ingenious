691129c649b3aad77ab4428c3be14615338e3453
MZ <>
2016-04-14 15:29:12 +0200

    init and implement Gamemode2

---
+++ src/main/java/nappydevelopment/nappy_the_ingenious/data/Question.java
@@ -0,0 +1,31 @@
+package nappydevelopment.nappy_the_ingenious.data;
+
+public class Question{
+
+	private String table;
+	private String attribute;
+	private String question;
+
+	private boolean isBoolean;
+
+	public Question(String table, String attribute, String question){
+		this.table = table;
+		this.attribute = attribute;
+		this.question = question;
+	}
+	public Question(String table, String attribute, String question, String type){
+		super();
+		this.isBoolean = type == "BOOLEAN";
+	}
+
+	public String getQuestion(){
+		return question;
+	}
+	public String getAttribute(){
+		return attribute;
+	}
+	public String getTable(){
+		return table;
+	}
+	public boolean isBoolean(){return isBoolean;}
+}
