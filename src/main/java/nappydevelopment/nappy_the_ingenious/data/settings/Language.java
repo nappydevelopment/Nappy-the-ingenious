41a6161adf07a3883b289f4d35e2c00dc79ff819
madblobfish <github@killerwolf.de>
2016-04-19 13:00:31 +0200

    improved language stuff in gamemode2

--- src/main/java/nappydevelopment/nappy_the_ingenious/data/settings/Language.java
+++ src/main/java/nappydevelopment/nappy_the_ingenious/data/settings/Language.java
@@ -1,7 +1,16 @@
 package nappydevelopment.nappy_the_ingenious.data.settings;
 //### IMPORTS ##############################################################################################################################
 public enum Language {
-	GERMAN, ENGLISH;
+	GERMAN("DE"),
+	ENGLISH("EN");
+
+	private String code;
+	Language(String c){
+		code = c;
+	}
+	public String getCode(){
+		return code;
+	}
 }
 
 //### EOF ##################################################################################################################################
