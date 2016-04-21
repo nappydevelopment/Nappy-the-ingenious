f48ef1d6f536b0d0de7fb6d8aae1768fcb5342a8
madblobfish <github@killerwolf.de>
2016-04-21 12:22:04 +0200

    language agnostic code in Characterprovider and WikiCharacter using the Language enums and stuff

--- src/main/java/nappydevelopment/nappy_the_ingenious/data/CharacterProvider.java
+++ src/main/java/nappydevelopment/nappy_the_ingenious/data/CharacterProvider.java
@@ -8,7 +8,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class CharacterProvider{
@@ -18,18 +20,27 @@ public class CharacterProvider{
 		List<WikiCharacter> out = new ArrayList<WikiCharacter>();
 		try{
 			if(st == null){return null;}
-			st.execute("SELECT name, description_en, description_de, nickname FROM SIMPSONS");
+			String select = "SELECT name";
+			for(Language l: Language.values()){
+				select += ", description_"+ l.getCode().toLowerCase();
+				select += ", nickname_"+ l.getCode().toLowerCase();
+			}
+			st.execute(select + "FROM SIMPSONS");
 			ResultSet res = st.getResultSet();
 
 			while(res.next()){
 				String name = res.getString("name");
-				String nickname = res.getString("nickname");
-				String description_en = res.getString("description_en");
-				String description_de = res.getString("description_de");
+				Map<Language, String> descriptions= new HashMap<>();
+				Map<Language, String> nicknames= new HashMap<>();
+
+				for(Language l: Language.values()){
+					descriptions.put(l, res.getString("description_"+ l.getCode().toLowerCase()));
+					nicknames.put(l, res.getString("description_"+ l.getCode().toLowerCase()));
+				}
 
 				Image img = new Image(GlobalReferences.IMAGES_PATH + "wiki/" + name.toLowerCase().replace(" ", "_") +".png");
 
-				WikiCharacter chr = new WikiCharacter(name, nickname, description_de, description_en, img);
+				WikiCharacter chr = new WikiCharacter(name, nicknames, descriptions, img);
 				out.add(chr);
 			}
 			st.close();
