af7bb4041c75a0386c0175d9d36c8e4337731d45
Manuel Bothner <manuel.bothner@web.de>
2016-04-28 09:51:21 +0200

    Refactoring

--- src/main/java/nappydevelopment/nappy_the_ingenious/data/settings/GameMode.java
+++ src/main/java/nappydevelopment/nappy_the_ingenious/data/settings/GameMode.java
@@ -1,7 +1,17 @@
 package nappydevelopment.nappy_the_ingenious.data.settings;
 //### IMPORTS ##############################################################################################################################
 public enum GameMode {
-	BOTH_MODES, ONLY_MODE1;
+	BOTH_MODES(true), ONLY_MODE1(false);
+	
+	private final boolean afgm2;
+	
+	private GameMode(boolean afgm2) {
+		this.afgm2 = afgm2;
+	}
+	
+	public boolean askForGamemode2() {
+		return this.afgm2;
+	}
 }
 
 //### EOF ##################################################################################################################################
