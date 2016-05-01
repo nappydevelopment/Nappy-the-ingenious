f6c02e09cafbfd96b0353d88a2d1158f28aab10e
Uindemia <marc@m-mahler.de>
2016-05-01 14:30:26 +0200

    Added a CharFilter Class

---
+++ src/main/java/nappydevelopment/nappy_the_ingenious/data/CharFilter.java
@@ -0,0 +1,7 @@
+package nappydevelopment.nappy_the_ingenious.data;
+
+/**
+ * Created by Marc on 01.05.2016.
+ */
+public class CharFilter {
+}
--- src/main/java/nappydevelopment/nappy_the_ingenious/data/WikiStageFilter.java
+++ src/main/java/nappydevelopment/nappy_the_ingenious/data/WikiStageFilter.java
@@ -1,9 +1,14 @@
 package nappydevelopment.nappy_the_ingenious.data;
 
+import java.util.List;
+
 /**
  * Created by Marc on 01.05.2016.
  */
 public class WikiStageFilter {
 
+    public List<WikiCharacter> sortListByFilter(List<WikiCharacter> chars, CharFilter filter){
+        return null;
+    }
 
 }
