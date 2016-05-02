14d12d9ce9314fcc0dcb69d4a74463343f6dbd73
Mehmet Ali Incekara <xnappy@web.de>
2016-05-02 15:29:43 +0200

    Update custom exception for changing the language

--- src/main/java/nappydevelopment/nappy_the_ingenious/exception/ChangeLanguageException.java
+++ src/main/java/nappydevelopment/nappy_the_ingenious/exception/ChangeLanguageException.java
@@ -3,7 +3,7 @@ package nappydevelopment.nappy_the_ingenious.exception;
 /**
  * Created by mincekara on 02.05.2016.
  */
-public class ChangeLanguageException extends RuntimeException{
+public class ChangeLanguageException extends Exception{
 
     public ChangeLanguageException(){
         super();
