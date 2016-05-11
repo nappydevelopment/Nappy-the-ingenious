068ccbeb5e85623ae612830ba68e6eabf2c047d5
Uindemia <marc@m-mahler.de>
2016-05-11 18:08:06 +0200

    Stuff for Nelson Easteregg

---
+++ src/main/java/nappydevelopment/nappyTheIngenious/util/eastereggs/EastereggSearcher.java
@@ -0,0 +1,19 @@
+package nappydevelopment.nappyTheIngenious.util.eastereggs;
+
+/**
+ * Created by Marc on 11.05.2016.
+ */
+public class EastereggSearcher {
+
+    public static void lookForEasteregg(String str){
+        if(str.equals("haha")){
+            NelsonFrame.createNewNelson();
+        }
+        else if(str.equals("doh") || str.equals("d'oh")) {
+
+        }
+        else if(str.equals("nappy")) {
+
+        }
+    }
+}
