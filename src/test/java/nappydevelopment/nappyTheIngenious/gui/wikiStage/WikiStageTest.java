d08ef5a1df44f6e2fba9f8f8294f1a1591406c1e
MZ <>
2016-05-19 16:15:42 +0200

    WikiStageTest: test filter

---
+++ src/test/java/nappydevelopment/nappyTheIngenious/gui/wikiStage/WikiStageTest.java
@@ -0,0 +1,67 @@
+package nappydevelopment.nappyTheIngenious.gui.wikiStage;
+
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import nappydevelopment.nappyTheIngenious.data.character.Age;
+import nappydevelopment.nappyTheIngenious.data.character.Character;
+import nappydevelopment.nappyTheIngenious.data.character.CharacterImage;
+import nappydevelopment.nappyTheIngenious.data.character.Gender;
+import nappydevelopment.nappyTheIngenious.data.settings.Language;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.assertEquals;
+
+public class WikiStageTest extends ApplicationTest{
+	private WikiStageController controller;
+
+	private List<Character> chars = new ArrayList();
+	private Map<Language, String> nicknames = new HashMap<>();
+
+	@Override
+	public void init() throws TimeoutException{
+		nicknames.put(Language.GERMAN, "nickname");
+
+		Image img = new CharacterImage("lisa simpson").get();
+		chars.add(new Character("lisa simpson", nicknames, nicknames, img, Gender.FEMALE, Age.YOUNG));
+		chars.add(new Character("homer simpson", nicknames, nicknames, img, Gender.MALE, Age.ADULT));
+		chars.add(new Character("Burns", nicknames, nicknames, img, Gender.MALE, Age.OLD));
+
+		controller = new WikiStageController();
+
+		FxToolkit.registerStage(() -> {
+			controller.initView(chars);
+			controller.changeLanguageToGerman();
+			return controller.view;
+		});
+	}
+	@Override
+	public void start(Stage stage){ stage.show(); }
+	@Override
+	public void stop(){ controller.view.close(); }
+
+	@Test
+	public void filterYoung(){
+		clickOn(controller.view.rbtYoung);
+		assertEquals(
+			controller.view.vbxContentPane.getChildren().get(0).lookup(".lblName"),
+			lookup(".lblName").query()
+		);
+	}
+	@Test
+	public void filterYoungFemale(){
+		clickOn(controller.view.rbtYoung);
+		clickOn(controller.view.rbtFemale);
+		assertEquals(
+			controller.view.vbxContentPane.getChildren().get(0).lookup(".lblName"),
+			lookup(".lblName").query()
+		);
+	}
+}
