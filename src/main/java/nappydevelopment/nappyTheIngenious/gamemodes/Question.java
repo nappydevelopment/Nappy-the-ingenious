0a7d575ef207efff016e38ef5597ade3caf9f4f0
MZ <>
2016-05-20 11:55:17 +0200

    fix gm1; question.attributeAnswered(Q) did use attribute by fault; table is correct

--- src/main/java/nappydevelopment/nappyTheIngenious/gamemodes/Question.java
+++ src/main/java/nappydevelopment/nappyTheIngenious/gamemodes/Question.java
@@ -47,7 +47,7 @@ public class Question{
 		this.answer = answer;
 	}
 	public Question attributeAnswered(Question q){
-		if((q.getAnswer() == Answer.YES || bool) && attribute.equals(q.getAttribute())){
+		if((q.getAnswer() == Answer.YES || bool) && table.equals(q.getTable())){
 			attributeAnswered = true;
 		}
 		return this; // for predicate usage
