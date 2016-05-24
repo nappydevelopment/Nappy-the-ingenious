668b17a290dd347d78d4957e9691b1d3e3e7c0a7
madblobfish <github@killerwolf.de>
2016-05-25 00:46:24 +0200

    QuestAnsList: optmize setAnswer and add sorting

--- src/main/java/nappydevelopment/nappyTheIngenious/data/QuestAnsList.java
+++ src/main/java/nappydevelopment/nappyTheIngenious/data/QuestAnsList.java
@@ -1,24 +1,17 @@
 package nappydevelopment.nappyTheIngenious.data;
 
 import java.util.LinkedList;
-import java.util.ListIterator;
 
 //### IMPORTS ##############################################################################################################################
 public class QuestAnsList extends LinkedList<QuestAnsElement>{
 
 	public void setAnswer(String question, Answer answer) {
-		
-        ListIterator<QuestAnsElement> listIterator = this.listIterator();
-        
-        while (listIterator.hasNext()) {
-        	
-        	//Read out current character:
-        	QuestAnsElement qae = listIterator.next();
-        	
-        	if(qae.getQuestion() == question) {
-        		qae.setAnswer(answer);
-        	}
-        }
+		this.stream()
+			.filter(qae -> question.equals(qae.getQuestion()))
+			.forEach(qae -> qae.setAnswer(answer));
+		this.sort(
+			(a, b) -> Boolean.compare(a.getAnswer() == null, b.getAnswer() == null)
+		);
 	}
 }
 //### EOF ##################################################################################################################################
