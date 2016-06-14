066333a5fdccd3b6d46f1f44aa2f005e5387a847
Uindemia <marc@m-mahler.de>
2016-06-14 14:16:24 +0200

    Finished the Nappy Easteregg and deleted the Nappy Stage

--- src/main/java/nappydevelopment/nappyTheIngenious/util/eastereggs/SoundPlayer.java
+++ src/main/java/nappydevelopment/nappyTheIngenious/util/eastereggs/SoundPlayer.java
@@ -21,19 +21,6 @@ public class SoundPlayer {
         }
     }
 
-    public SoundPlayer(String str, NappyStage stage){
-        try {
-            String bip = Thread.currentThread().getContextClassLoader().getResource(GlobalReferences.SOUND_PATH + str + ".mp3").toURI().toString();
-            Media hit = new Media(bip);
-            mediaPlayer = new MediaPlayer(hit);
-            mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-            mediaPlayer.play();
-
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-    }
-
     public void stopCurrentTune(){
         try {
             mediaPlayer.stop();
